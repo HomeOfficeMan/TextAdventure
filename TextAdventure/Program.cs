8299cd419e235d077286f53d733644bd14d2e94c
HomeOfficeMan <julianolberg@gmail.com>
2025-01-26 11:46:03 +1300

    Create Dev Branch

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -5,7 +5,6 @@ class Program
     static void Main()
     {
         Console.WriteLine("Game Starts");
-        GameManger.Instance.RunGame();
-        
-    }
+        GameManger.Instance.RunGame();       
+    } 
 }
