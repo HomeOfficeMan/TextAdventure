7936cacae381f3a1088406dfad744b746800dea2
HomeOfficeMan <julianolberg@gmail.com>
2025-01-26 12:20:58 +1300

    Gamestate changer

--- TextAdventure/GameManager.cs
+++ TextAdventure/GameManager.cs
@@ -1,23 +1,62 @@
 public class GameManger
 {
     private static GameManger? instance;
+    public GameState currentGameState { get; private set; }
 
     public static GameManger Instance
     {
         get
         {
-            // Lazy initialization
             if (instance == null)
             {
                 instance = new GameManger();
             }
+
+            instance.ChangeState(GameState.Starting);
             return instance;
         }
     }
 
+    public enum GameState
+    {
+        Starting,
+        Menu,
+        Playing,
+        Pause,
+        GameOver
+    }
+
+    // private Player player;
+    // private CreatureManager creatureManger;
+    // private StringManager stringManager;
+
     public void RunGame()
     {
         Console.WriteLine("Game Started!");
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        currentGameState = newState;
+
+        switch (newState)
+        {
+            case GameState.Starting:
+                Console.WriteLine("Game is starting");
+                break;
+            case GameState.Menu:
+                Console.WriteLine("Game in Menu state");
+                break;
+            case GameState.Playing:
+                Console.WriteLine("Game started!");
+                break;
+            case GameState.Pause:
+                Console.WriteLine("Game paused");
+                break;
+            case GameState.GameOver:
+                Console.WriteLine("Game over");
+                break;            
+        }
     }
 
     void InitilizeNewGame()
--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Game Starts");
         GameManger.Instance.RunGame();       
     } 
 }
